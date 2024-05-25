11b048952cc16a9284f9c5ea139f7d2bf2025a0b
YukiNoguchi <126850727+MA-8888@users.noreply.github.com>
2024-05-25 18:30:32 +0900

    a

---
+++ src/main/java/com/example/demo3/entity/Posts.java
@@ -0,0 +1,10 @@
+package com.example.demo3.entity;
+
+import lombok.Data;
+
+@Data
+public class Posts {
+	private Long id;
+	private String title;
+	private String name;
+}
---
+++ src/main/java/com/example/demo3/repository/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo3.repository;
+
+import com.example.demo3.entity.Posts;
+
+public interface PostsRepository extends JpaRepository <Posts, String> {
+
+}
